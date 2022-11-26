9a4b9f6cfb22cacc00f5ae2d489f206b714f30c1
Tim <tim@mensinger96.com>
2022-11-26 14:25:21 +0100

    Update task_analysis

--- src/functional_treatment_effects/analysis/task_analysis.py
+++ src/functional_treatment_effects/analysis/task_analysis.py
@@ -6,72 +6,86 @@ from functional_treatment_effects.config import BLD
 from functional_treatment_effects.data_management import INDEX_COLS
 
 
-fit_func_on_scalar = get_fitter(fitter="func_on_scalar")
-fit_doubly_robust = get_fitter(fitter="func_on_scalar_doubly_robust")
-
-
 @pytask.mark.depends_on(
     {
-        "x": BLD.joinpath("data/bare/strike_indicator.csv"),
-        "y": BLD.joinpath("data/bare/ankle_moments.csv"),
+        "treatment_status": BLD.joinpath("data", "bare", "strike_indicator.csv"),
+        "y": BLD.joinpath("data", "bare", "ankle_moments.csv"),
     }
 )
-@pytask.mark.produces(BLD.joinpath("models/bare/coef.csv"))
-def task_fit_model(depends_on, produces):
-    x_index_cols = list(set(INDEX_COLS["strike_indicator"]) - {"shoe_type"})
-    y_index_cols = list(set(INDEX_COLS["ankle_moments"]) - {"shoe_type"})
-    x = pd.read_csv(depends_on["x"], index_col=x_index_cols)
-    y = pd.read_csv(depends_on["y"], index_col=y_index_cols)
+@pytask.mark.produces(BLD.joinpath("models", "bare", "linear_model.pickle"))
+def task_fit_linear_model(depends_on, produces):
+    # read data
+    # ==================================================================================
+    treatment_status = pd.read_csv(depends_on["treatment_status"], index_col=["id"])
+    y = pd.read_csv(depends_on["y"], index_col=["variable", "id"])
     y = y.query("variable == 'x'")
 
-    res = fit_func_on_scalar(x=x, y=y, fit_intercept=True)
-    res["slopes"].to_csv(produces)
+    # fit model
+    # ==================================================================================
+    fit_method = get_fitter(fitter="func_on_scalar")
+
+    res = fit_method(x=treatment_status, y=y, fit_intercept=True)
+
+    # write results
+    # ==================================================================================
+    pd.to_pickle(res, produces)
 
 
 @pytask.mark.depends_on(
     {
-        "x": BLD.joinpath("data/bare/subject_information.csv"),
-        "t": BLD.joinpath("data/bare/strike_indicator.csv"),
-        "y": BLD.joinpath("data/bare/ankle_moments.csv"),
+        "treatment_status": BLD.joinpath("data", "bare", "strike_indicator.csv"),
+        "x": BLD.joinpath("data", "bare", "subject_information.csv"),
+        "y": BLD.joinpath("data", "bare", "ankle_moments.csv"),
     }
 )
-@pytask.mark.produces(BLD.joinpath("models/bare/doubly_robust.csv"))
+@pytask.mark.produces(BLD.joinpath("models", "bare", "doubly_robust.pickle"))
 def task_fit_doubly_robust(depends_on, produces):
-    t_index_cols = list(set(INDEX_COLS["strike_indicator"]) - {"shoe_type"})
-    y_index_cols = list(set(INDEX_COLS["ankle_moments"]) - {"shoe_type"})
-    t = pd.read_csv(depends_on["t"], index_col=t_index_cols)
-    y = pd.read_csv(depends_on["y"], index_col=y_index_cols)
+    # read data
+    # ==================================================================================
+    treatment_status = pd.read_csv(depends_on["treatment_status"], index_col=["id"])
+
     x = pd.read_csv(depends_on["x"], index_col=INDEX_COLS["subject_information"])
+    x = x.drop(columns=["name"])
+
+    y = pd.read_csv(depends_on["y"], index_col=["variable", "id"])
     y = y.query("variable == 'x'")
 
-    controls = [
-        "gender",
-        "foot_shape",
-        "age",
-        "weekly_km",
-        "toe_flex_strength",
-        "plantar_flex_strength",
-        "knee_exten_strength",
-    ]
-    x = x[controls]
+    # data preparation
+    # ==================================================================================
     x = pd.get_dummies(x).dropna()
-
     y = y.droplevel(level="variable", axis=0).loc[x.index]
-    t = t.loc[x.index]
+    treatment_status = treatment_status.loc[x.index]
 
-    res = fit_doubly_robust(x=x, y=y, t=t)
+    # fit model
+    # ==================================================================================
+    fit_method = get_fitter(fitter="func_on_scalar_doubly_robust")
 
+    res = fit_method(x=x, y=y, t=treatment_status)
+
+    # write results
+    # ==================================================================================
+    pd.to_pickle(res, produces)
+
+
+@pytask.mark.depends_on(BLD.joinpath("models", "bare", "doubly_robust.pickle"))
+@pytask.mark.produces(BLD.joinpath("models", "bare", "doubly_robust.csv"))
+def task_compute_confidence_band(depends_on, produces):
+    # read data
+    # ==================================================================================
+    res = pd.read_pickle(depends_on)
+
+    # compute confidence band
+    # ==================================================================================
     effect = res["treatment_effect"]
     band = estimate_confidence_band(
         estimate=effect.values.flatten(),
-        cov=res["cov"] / len(y),
-        n_samples=len(y),
+        cov=res["cov"] / res["n_samples"],
+        n_samples=res["n_samples"],
         numerical_options={"raise_error": False},
     )
 
-    effect["lower"] = band.lower
-    effect["upper"] = band.upper
-    effect["estimate"] = band.estimate
+    res = pd.DataFrame(band._asdict()).set_index(effect.index)
 
-    effect = effect.drop(columns=["value"])
-    effect.to_csv(produces)
+    # write results
+    # ==================================================================================
+    res.to_csv(produces)
