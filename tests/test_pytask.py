5d151f8695986ca72a7c125ade091d613b5fc5e1
Tim <tim@mensinger96.com>
2022-11-28 20:28:28 +0100

    Dont run test_pytask on windows

--- tests/test_pytask.py
+++ tests/test_pytask.py
@@ -1,8 +1,11 @@
+import sys
+
 import functional_treatment_effects.config as config
 import pytask
 import pytest
 
 
+@pytest.mark.skipif(sys.platform == "win32", reason="pytask-marp does not run on win.")
 @pytest.mark.end_to_end
 def test_pytask_call(tmp_path, monkeypatch):
     monkeypatch.setattr(config, "BLD", tmp_path.joinpath("bld").resolve())
